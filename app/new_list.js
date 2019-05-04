cf96d444342be87aa671e3226106c7a16a35e8ab
RyBo <ryanccrawford@live.com>
2019-05-04 09:07:06 -0400

    Maps is integrated and works good

--- app/new_list.js
+++ app/new_list.js
@@ -217,23 +217,23 @@ function createMap() {
     
 
     mash_getUserWalmartStore(user.id)
+ var map;
 
+ function initMap() {
+     map = new google.maps.Map(document.getElementById('map'), {
+         center: {
+             lat: lat,
+             lng: lon
+         },
+         zoom: 8
+     });
+ }
     $(document).on('getWalmartStores', function (data) {
         console.log(data.message)
         var cords = data.message[0].coordinates
         lat = cords[0];
         lon = cords[1];
-        var map;
-
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: lat,
-                    lng: lon
-                },
-                zoom: 8
-            });
-        }
+       
          var mapssrc = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDiaHiIDgafsFhfwb1XQBtKETZ1zdlrP_o&callback=initMap';
         $.getScript(mapssrc, function (data, status, jqxhr) {
             console.log(data)
