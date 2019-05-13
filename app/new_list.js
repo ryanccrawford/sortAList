6f87311003e53f14f1e51fb460161faa9dbe5ab3
Ryan Crawford <RyanCCrawford@live.com>
2019-05-13 18:28:46 -0400

    update

--- app/new_list.js
+++ app/new_list.js
@@ -73,7 +73,8 @@ $(document).ready(function () {
 
         if (validateItems()) {
             var listTitle = $('#newtitle').val()
-            $('#listdisplay').empty()
+            var tt = $('#listdisplay')
+            $(tt).html("")
             var area = $('#itemaddarea')
             var childeren = $(area)[0].childNodes
             var itemsToAdd = inputNodesToList(childeren)
